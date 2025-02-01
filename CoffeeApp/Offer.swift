13a49139f4aefef01272d954b492b7b8cac90b64
lucerogoga <lucerogoga63@hotmail.com>
2025-01-31 18:44:22 -0800

    first commit

---
+++ CoffeeApp/Offer.swift
@@ -0,0 +1,18 @@
+//
+//  Offer.swift
+//  CoffeeApp
+//
+//  Created by Lucero Gonzales on 31/01/25.
+//
+
+import SwiftUI
+
+struct Offer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Offer()
+}

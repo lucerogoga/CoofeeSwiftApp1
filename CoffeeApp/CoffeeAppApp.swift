0cfa7d6da4800d7d7c4611499c65331668c54a7f
lucerogoga <lucerogoga63@hotmail.com>
2025-01-31 18:37:28 -0800

    Initial Commit

---
+++ CoffeeApp/CoffeeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeAppApp.swift
+//  CoffeeApp
+//
+//  Created by Lucero Gonzales on 31/01/25.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoffeeApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoffeeApp
+//
+//  Created by Lucero Gonzales on 31/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
